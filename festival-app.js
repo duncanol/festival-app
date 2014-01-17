b1e11f1cce9decf2b6b2d2192c5cfc64742a6590
Duncan Watson <duncan.watson@technophobia.com>
2014-01-17 16:43:20 +0000

    Authorship added

--- festival-app.js
+++ festival-app.js
@@ -2,15 +2,36 @@ chathistory = new Meteor.Collection("chat")
 
 if (Meteor.isClient) {
 
+    getUserName = function(userId) {
+        return Meteor.users.findOne({_id: userId}).username;
+    }
+    
+    Accounts.ui.config({
+        passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+    });
+    
     Template.chathistory.messages = function() {
-        return chathistory.find({});
+        var chatItems = chathistory.find({}).fetch();
+        
+        for (var i = 0; i < chatItems.length; i++) {
+            if (chatItems[i].author != null) {
+                chatItems[i].username = getUserName(chatItems[i].author);
+            } else {
+                chatItems[i].username = 'anon';
+            }
+        }
+        
+        return chatItems;
     }
     
     Template.chathistory.events({
         'keyup #chat-text': function(e) {
             if (e.which === 13) {
                 var text = e.target.value;
-                chathistory.insert({text: text});
+                chathistory.insert({
+                    text: text,
+                    author: Meteor.userId()
+                });
                 e.target.value = "";
             }
         },
